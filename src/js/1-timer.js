a96d95bb152d66c98ae812088db7704586433b32
OLyaNag <olyasikaa@gmail.com>
2024-06-09 12:22:23 +0300

    'update'

---
+++ src/js/1-timer.js
@@ -0,0 +1,6 @@
+'use strict'
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const flatpicktEl = document.querySelector('input#datetime-picker')
+console.log(flatpicktEl);
